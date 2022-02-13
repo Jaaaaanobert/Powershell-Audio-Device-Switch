fbe9d07bf10e84448a013c50322a585ce1104e21
Jaaaaanobert <68434157+Jaaaaanobert@users.noreply.github.com>
2022-02-14 00:26:09 +0100

    Update Device_Switch_General.ps1

--- Device_Switch_General.ps1
+++ Device_Switch_General.ps1
@@ -1,10 +1,28 @@
-$idSpeaker = "YOUR SPEAKER-ID"
-$idHeadphones = "YOUR HEADHONE-ID"
+$idSpeaker = "YOUR SPEAKER ID HERE"
+$idHeadphones = "YOUR HEADPHONES ID HERE"
 
 if((Get-AudioDevice -List | where {($_.Default -eq $true) -and ($_.Type -like "Playback") -and ($_.ID -like $idSpeaker )}).Default -like "true") {
     # Switch to Speaker
     Set-AudioDevice -ID $idHeadphones -Verbose | Out-Null
+    Notificiation("speaker")
+
 } elseif((Get-AudioDevice -List | where {($_.Default -eq $true) -and ($_.Type -like "Playback") -and ($_.ID -like $idHeadphones )}).Default -like "true") {
     # Switch to Headset
     Set-AudioDevice -ID $idSpeaker -Verbose | Out-Null
+    Notificiation("headphones")
+}
+
+Function Notificiation([string]$name) {
+
+
+    Add-Type -AssemblyName System.Windows.Forms
+$global:balmsg = New-Object System.Windows.Forms.NotifyIcon
+$path = (Get-Process -id $pid).Path
+$balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+$balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
+$balmsg.BalloonTipText = "Switched Audio device to " + $name
+$balmsg.BalloonTipTitle = "Audio Device Switch"
+$balmsg.Visible = $true
+$balmsg.ShowBalloonTip(2000)
+    
 }
