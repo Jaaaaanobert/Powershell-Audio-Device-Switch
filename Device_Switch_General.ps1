bd643efa1e0a772619725c885f4445fc7fc3278c
Jaaaaanobert <68434157+Jaaaaanobert@users.noreply.github.com>
2022-02-14 00:30:52 +0100

    Update Device_Switch_General.ps1

--- Device_Switch_General.ps1
+++ Device_Switch_General.ps1
@@ -1,17 +1,3 @@
-$idSpeaker = "YOUR SPEAKER ID HERE"
-$idHeadphones = "YOUR HEADPHONES ID HERE"
-
-if((Get-AudioDevice -List | where {($_.Default -eq $true) -and ($_.Type -like "Playback") -and ($_.ID -like $idSpeaker )}).Default -like "true") {
-    # Switch to Speaker
-    Set-AudioDevice -ID $idHeadphones -Verbose | Out-Null
-    Notificiation("speaker")
-
-} elseif((Get-AudioDevice -List | where {($_.Default -eq $true) -and ($_.Type -like "Playback") -and ($_.ID -like $idHeadphones )}).Default -like "true") {
-    # Switch to Headset
-    Set-AudioDevice -ID $idSpeaker -Verbose | Out-Null
-    Notificiation("headphones")
-}
-
 Function Notificiation([string]$name) {
 
 
@@ -25,4 +11,18 @@ $balmsg.BalloonTipTitle = "Audio Device Switch"
 $balmsg.Visible = $true
 $balmsg.ShowBalloonTip(2000)
     
+}
+
+$idSpeaker = "{0.0.0.00000000}.{6eb60296-bf16-4c6e-aa23-524a5d6d2949}"
+$idHeadphones = "{0.0.0.00000000}.{a7b74d86-6712-4ea9-901a-8dd769abbee7}"
+
+if((Get-AudioDevice -List | where {($_.Default -eq $true) -and ($_.Type -like "Playback") -and ($_.ID -like $idSpeaker )}).Default -like "true") {
+    # Switch to Speaker
+    Set-AudioDevice -ID $idHeadphones -Verbose | Out-Null
+    Notificiation("speaker")
+
+} elseif((Get-AudioDevice -List | where {($_.Default -eq $true) -and ($_.Type -like "Playback") -and ($_.ID -like $idHeadphones )}).Default -like "true") {
+    # Switch to Headset
+    Set-AudioDevice -ID $idSpeaker -Verbose | Out-Null
+    Notificiation("headphones")
 }
