2a42e44d123841b1ba35b6e79d70ef671c03b345
Jaaaaanobert <68434157+Jaaaaanobert@users.noreply.github.com>
2022-02-14 09:01:30 +0100

    Update Device_Switch_General.ps1

--- Device_Switch_General.ps1
+++ Device_Switch_General.ps1
@@ -1,5 +1,9 @@
-Function Notificiation([string]$name) {
+$idSpeaker = "YOUR DEVICE ID"
+$idHeadphones = "YOUR DEVICE ID"
 
+Function Notificiation([string]$name) {
+$idSpeaker = "{0.0.0.00000000}.{6eb60296-bf16-4c6e-aa23-524a5d6d2949}"
+$idHeadphones = "{0.0.0.00000000}.{a7b74d86-6712-4ea9-901a-8dd769abbee7}"
 
     Add-Type -AssemblyName System.Windows.Forms
 $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
@@ -13,8 +17,6 @@ $balmsg.ShowBalloonTip(2000)
     
 }
 
-$idSpeaker = "{0.0.0.00000000}.{6eb60296-bf16-4c6e-aa23-524a5d6d2949}"
-$idHeadphones = "{0.0.0.00000000}.{a7b74d86-6712-4ea9-901a-8dd769abbee7}"
 
 if((Get-AudioDevice -List | where {($_.Default -eq $true) -and ($_.Type -like "Playback") -and ($_.ID -like $idSpeaker )}).Default -like "true") {
     # Switch to Speaker
